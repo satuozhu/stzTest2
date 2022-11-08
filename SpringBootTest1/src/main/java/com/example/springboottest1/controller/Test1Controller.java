57f1e28e7d5e726490854af5ba9678118cbe11a3
satuozhu <15527109467@163.com>
2022-11-08 16:47:11 +0800

    16:47

--- SpringBootTest1/src/main/java/com/example/springboottest1/controller/Test1Controller.java
+++ SpringBootTest1/src/main/java/com/example/springboottest1/controller/Test1Controller.java
@@ -36,4 +36,10 @@ public class Test1Controller {
         return "hello springboot test4";
     }
 
+    @RequestMapping("/test5")
+    @ResponseBody
+    public String hello5(){
+        return "hello springboot test5";
+    }
+
 }
