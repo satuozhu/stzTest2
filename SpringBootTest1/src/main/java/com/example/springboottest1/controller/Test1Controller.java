fbed8ccab7e2ea421629192ff8b4d92842f967b7
satuozhu <15527109467@163.com>
2022-11-19 15:28:34 +0800

    19:49

--- SpringBootTest1/src/main/java/com/example/springboottest1/controller/Test1Controller.java
+++ SpringBootTest1/src/main/java/com/example/springboottest1/controller/Test1Controller.java
@@ -3,35 +3,32 @@ package com.example.springboottest1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author：Lucas
  * @date: 2022/11/5 10:10
  * @description: some description
  */
-@Controller
+@RestController
 @RequestMapping("/boot")
 public class Test1Controller {
 
 
     @RequestMapping("/test1")
-    @ResponseBody
     public String hello(){
         return "hello springboot test";
     }
 
     @RequestMapping("/test2")
-    @ResponseBody
     public String hello2(){
         return "hello springboot test2";
     }
     @RequestMapping("/test3")
-    @ResponseBody
     public String hello3(){
         return "hello springboot test3";
     }
     @RequestMapping("/test4")
-    @ResponseBody
     public String hello4(){
         return "hello springboot test4";
     }
